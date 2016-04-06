ff8b6cfc3e105c8e5810a3669522372a214e7eef
Kronosfear <vijayrajasekar1@gmail.com>
2016-04-06 05:58:39 +0530

    Zero Cross
    added zero crossing

--- VoiceAnalyser/Analyse2.py
+++ VoiceAnalyser/Analyse2.py
@@ -1,11 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb 20 00:32:28 2016
-
-@author: Vijay
-"""
-
-# Read in a WAV and find the freq's
 import pyaudio
 import wave
 import numpy as np
@@ -13,7 +5,7 @@ import numpy as np
 chunk = 2048
 
 # open up a wave
-wf = wave.open('male/demo.wav', 'rb')
+wf = wave.open('male/yes.wav', 'rb')
 swidth = wf.getsampwidth()
 RATE = wf.getframerate()
 # use a Blackman window
@@ -28,19 +20,15 @@ stream = p.open(format =
 
 # read some data
 data = wf.readframes(chunk)
-
-
 # play stream and find the frequency of each chunk
 while len(data) == chunk*swidth:
     # write data out to the audio stream
     stream.write(data)
-
-
     # unpack the data and times by the hamming window
     indata = np.array(wave.struct.unpack("%dh"%(len(data)/swidth),\
                                          data))*window
     # Take the fft and square each value
-    fftData=abs(np.fft.rfft(indata))**2
+    fftData=abs(np.fft.rfft(indata))^2
     # find the maximum
     which = fftData[1:].argmax() + 1
     # use quadratic interpolation around the max
@@ -49,15 +37,13 @@ while len(data) == chunk*swidth:
         x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
         # find the frequency and output it
         thefreq = (which+x1)*RATE/chunk
-        print ("The freq is %f Hz.", thefreq)
+        print ("The freq is ", thefreq)
     else:
         thefreq = which*RATE/chunk
-        print ("The freq is %f Hz.", thefreq)
-        
+        print ("The freq is ", thefreq)
     # read some more data
     data = wf.readframes(chunk)
 if data:
     stream.write(data)
-
 stream.close()
 p.terminate()
--- VoiceAnalyser/Analyse3.py
+++ VoiceAnalyser/Analyse3.py
@@ -10,10 +10,26 @@ import wave
 import struct
 import scipy.fftpack
 import math
+import pyaudio
+
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 5
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                frames_per_buffer=CHUNK)
 
 
 
-def plotSpectru(y,Fs):
+def FFTSpectrum(y, Fs):
     n = len(y)
     k = arange(n)
     T = n/Fs
@@ -29,7 +45,7 @@ def plotSpectru(y,Fs):
 def TimeAmpSpectrum(filename):
     rate,data=read(filename)
     y=data[:]
-    lungime=len(y)
+    l=len(y)
     timp=len(y)/44100.
     t=linspace(0,timp,len(y))
     subplot(2,1,1)
@@ -37,16 +53,10 @@ def TimeAmpSpectrum(filename):
     xlabel('Time')
     ylabel('Amplitude')
     subplot(2,1,2)
-    plotSpectru(y,Fs)
+    FFTSpectrum(y,Fs)
     show()
 
 
-def zerocrossings(signal, RATE):
-    signal = numpy.fromstring(signal, 'Int16');
-    crossing = [math.copysign(1.0, s) for s in signal]
-    index = find(numpy.diff(crossing));
-    f0=round(len(index) * RATE /(2*numpy.prod(len(signal))))
-    return f0;
 
 
 def fft_cepstral(filename):
@@ -63,7 +73,7 @@ def fft_cepstral(filename):
     
     ceps=ifft(w);
     posmax = ceps.argmax();
-    result = Fs/(64*nframes*nchannels)*(posmax-1) 
+    result = Fs/(32*nframes*nchannels)*(posmax-1) 
     print("Cepstral: ", result)
     
     freqs = numpy.fft.fftfreq(len(w))
@@ -73,24 +83,19 @@ def fft_cepstral(filename):
     print("FFT: ", freq_in_hertz)
 
 
-def Pitch(signal):
-    signal = np.fromstring(signal, 'Int16');
-    crossing = [math.copysign(1.0, s) for s in signal]
-    index = find(np.diff(crossing));
-    f0=round(len(index) *RATE /(2*np.prod(len(signal))))
-    return f0;
-
-
 def zerocross(filename):
-    for i in range(0, RATE / chunk * RECORD_SECONDS):
-    data = stream.read(chunk)
-    Frequency=Pitch(data)
-    print "%f Frequency" %Frequency
+    sig=read(filename)
+    y= numpy.array(sig)
+    indices = find((sig[1:] >= 0) & (sig[:-1] < 0))
+    crossings = [i - sig[i] / (sig[i+1] - sig[i]) for i in indices]
+    Frequency =  fs / mean(diff(crossings))
+    print ("Zero Crossing:  ",Frequency)
 
     
     
 Fs = 11025;  # sampling rate
 
-filename = 'male/no.wav'
+filename = 'male/demo.wav'
 fft_cepstral(filename)
 
+TimeAmpSpectrum(filename)
